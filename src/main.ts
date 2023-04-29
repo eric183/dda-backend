d9fa870d6f026eceff61a767241ae8d35c657d2d
wen mac <kk297466058@gmail.com>
2023-04-29 12:34:48 +0800

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
