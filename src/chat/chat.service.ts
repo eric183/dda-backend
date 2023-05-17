8be3447aabef6c34dad65c29205bced1f613b26e
eric183 <kk297466058@gmail.com>
2023-05-18 00:59:12 +0800

    updated

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -6,12 +6,27 @@ import { Socket } from 'socket.io';
 @Injectable()
 export class ChatService {
   // 用户id到socket实例的映射
-  private userSocketMap = new Map<string, Socket>();
+  private userSocketMap = new Map<
+    string,
+    {
+      socket?: Socket;
+      offlineMessages: any[];
+    }
+  >();
   server: any;
 
   async setUserSocket(id: string, socket: Socket) {
-    this.userSocketMap.set(id, socket);
+    const socketMap = this.userSocketMap.get(id);
+    this.userSocketMap.set(id, {
+      socket,
+      offlineMessages:
+        socketMap && socketMap?.offlineMessages
+          ? socketMap.offlineMessages
+          : [],
+    });
   }
+
+  async helpToUserCreateSocketMap(userId: string) {}
 
   async getUserSocket(id: string) {
     return this.userSocketMap.get(id);
@@ -25,17 +40,19 @@ export class ChatService {
    * @param type: "demand" | "normal" 发起聊天请求 message
    */
   async startChat({ fromUserId, toUserId, message, demandId, type }) {
+    // this.helpToUserCreateSocketMap(toUserId);
     const toUserSocket = this.userSocketMap.get(toUserId);
 
     if (toUserSocket) {
       // 接收方在线，向接收方发起聊天请求
-      toUserSocket.emit('startChat', {
+      toUserSocket.socket.emit('startChat', {
         fromUserId,
         message,
         demandId,
         type,
       });
     } else {
+      console.log('离线');
       // 接收方离线，存储聊天请求并等待接收方上线
       // 此处可以根据实际业务需求拓展，比如将请求存储到数据库中
     }
@@ -49,7 +66,7 @@ export class ChatService {
    */
   async joinChat(fromUserId: string, toUserId: string, client: Socket) {
     // 将用户id和socket实例建立映射关系
-    this.userSocketMap.set(fromUserId, client);
+    this.setUserSocket(fromUserId, client);
     client.join(`${fromUserId}-${toUserId}`);
   }
 
@@ -70,9 +87,21 @@ export class ChatService {
    * @param message 消息内容
    */
   async sendMessage(fromUserId: string, toUserId: string, message: string) {
-    const targetClient = this.userSocketMap.get(toUserId);
+    // 待优化: 未上线用户逻辑
 
-    targetClient.emit('receiveMessage', {
+    const targetClient = this.userSocketMap.get(toUserId);
+    if (!targetClient) {
+      this.userSocketMap.set(toUserId, {
+        offlineMessages: [
+          {
+            fromUserId,
+            message,
+          },
+        ],
+      });
+    }
+
+    targetClient?.socket.emit('receiveMessage', {
       fromUserId,
       toUserId,
       message,
