ff80b88e66439e1531235ad6567bda1988a570ba
kk297466058@gmail.com <kk297466058@gmail.com>
2024-12-24 11:26:09 -0500

    :)

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: "dda-backend",
+    script: "./dist/src/main.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      NODE_ENV: "development",
+      PORT: 3000,
+    },
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3000
+    }
+  }]
+ }
